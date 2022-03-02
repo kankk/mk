8938b20be54d09c2d73069cf83787e009191099a
kankk <286454796@qq.com>
2022-03-03 01:07:16 +0800

    feat: docs

--- src/pages/Docs/DocsList.vue
+++ src/pages/Docs/DocsList.vue
@@ -1,8 +1,44 @@
 <script setup lang="ts">
+import { DocsGroups } from '@utils/const';
+
+const handleLinkClick = (link: string) => window.open(link);
 </script>
 
 <template>
-  <div>
-    Docs
+  <div class="px-10">
+    <div
+      v-for="group of DocsGroups"
+      :key="group.name"
+      class="mb-2"
+    >
+      <div class="text-xl font-medium px-4">
+        {{ group.name }}
+      </div>
+      <div
+        v-for="doc of group.docs"
+        :key="doc.name"
+        class="flex justify-between items-center px-4 py-1 hover:bg-slate-100"
+      >
+        <div class="flex items-center">
+          <MIcon
+            class="mr-2"
+            :component="doc.icon"
+            :width="20"
+            :height="20"
+          />
+          <div>{{ doc.name }}</div>
+        </div>
+        <div class="flex justify-between items-center">
+          <div
+            v-for="link of doc.links"
+            :key="link.name"
+            class="mr-4 last:mr-0 text-black/45 hover:text-black/85 hover:cursor-pointer"
+            @click="handleLinkClick(link.url)"
+          >
+            {{ link.name }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -2,6 +2,7 @@ import { DefineComponent } from 'vue';
 import BlogList from '@pages/Blog/BlogList.vue';
 import BookmarkList from '@pages/Bookmark/BookmarkList.vue';
 import DocsList from '@pages/Docs/DocsList.vue';
+import { IconVue, IconNest } from './icons';
 
 // 顶部导航居中链接
 export interface IHeaderLink {
@@ -15,3 +16,42 @@ export const HeaderLinks = [
   { name: 'Bookmark', path: '/bookmark', component: BookmarkList },
   { name: 'Docs', path: '/docs', component: DocsList },
 ] as Array<IHeaderLink>;
+
+// 文档链接
+export const DocsGroups = [
+  {
+    name: 'Vue',
+    docs: [
+      {
+        name: 'Vue3',
+        icon: IconVue,
+        links: [
+          { name: '中文', url: 'https://v3.cn.vuejs.org/guide' },
+          { name: 'github', url: 'https://github.com/vuejs/core' },
+        ],
+      },
+      {
+        name: 'Vue2',
+        icon: IconVue,
+        links: [
+          { name: '中文', url: 'https://cn.vuejs.org/v2/guide/' },
+          { name: 'github', url: 'https://github.com/vuejs/vue' },
+        ],
+      }
+    ],
+  },
+  {
+    name: 'Node',
+    docs: [
+      {
+        name: 'Nest',
+      icon: IconNest,
+        links: [
+          { name: '中文', url: 'https://docs.nestjs.cn/8/firststeps' },
+          { name: '英文', url: 'https://docs.nestjs.com/' },
+          { name: 'github', url: 'https://github.com/nestjs/nest'}
+        ]
+      }
+    ]
+  }
+];
